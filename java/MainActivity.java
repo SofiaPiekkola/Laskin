70c626fd4191cef57b3c49a760a496fc1bd7e918
SofiaPiekkola <sofia.piekkola@hotmail.com>
2017-10-05 21:17:15 +0300

    Update MainActivity.java

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,8 +1,5 @@
-package com.sofia.laskin2;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
@@ -10,57 +7,58 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView calculation;
-    TextView result;
+    TextView t1;
+    TextView t2;
     Utils utils;
-    HorizontalScrollView scroll1;
-    HorizontalScrollView scroll2;
+    HorizontalScrollView ho1;
+    HorizontalScrollView ho2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         utils = new Utils(this);
-        calculation = (TextView) findViewById(R.id.mid);
-        result = (TextView) findViewById(R.id.con);
-        calculation.setMovementMethod(new ScrollingMovementMethod());
-        scroll1 = (HorizontalScrollView) findViewById(R.id.scroll1);
-        scroll2 = (HorizontalScrollView) findViewById(R.id.scroll2);
+        t1 = (TextView) findViewById(R.id.mid);
+        t2 = (TextView) findViewById(R.id.con);
+        ho1 = (HorizontalScrollView) findViewById(R.id.scroll1);
+        ho2 = (HorizontalScrollView) findViewById(R.id.scroll2);
     }
 
     public void numberClicked(View view) {
         Button b = (Button) findViewById(view.getId());
-        String buttonTxt = b.getText().toString();
-        utils.numberClicked(buttonTxt);
+        utils.numberClicked(b.getText().toString());
     }
 
     public void commaClicked(View view) {
         Button b = (Button) findViewById(view.getId());
-        String buttonTxt = b.getText().toString();
-        utils.commaClicked(buttonTxt);
+        utils.commaClicked(b.getText().toString());
     }
 
     public void calculationClicked(View view) {
         Button b = (Button) findViewById(view.getId());
-        String buttonTxt = b.getText().toString();
-        utils.calculationClicked(buttonTxt);
+        utils.calculationClicked(b.getText().toString());
     }
 
-    public void cClicked(View view) { utils.cClicked();}
-
-    public void createToast(String message) { Toast.makeText(this, message, Toast.LENGTH_SHORT).show(); }
-
-    public String getCalculationText() { return calculation.getText().toString(); }
-
-    public void setResultText(String resultText) {
-        result.setText(resultText);
-        scroll2.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+    public void cClicked(View view) {
+        utils.cClicked();
     }
 
-    public void setCalculationText(String calculation, boolean append) {
-        if (append) this.calculation.append(calculation);
-        else this.calculation.setText(calculation);
-        scroll1.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+    public String getCalculationText(){
+        return t1.getText().toString();
+    }
+
+    public void setResultText(String text){
+        t2.setText(text);
+        ho2.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+    }
+
+    public void setCalculationText(String s, boolean add){
+        if (add)t1.append(s);
+        else t1.setText(s);
+        ho1.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+    }
+
+    public void createToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
